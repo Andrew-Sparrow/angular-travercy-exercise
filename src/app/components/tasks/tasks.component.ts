6cda7ab1140fdc6788d82965c29b450b80d0d5be
Andrew <8911102@gmail.com>
2023-01-02 21:46:58 +0500

    observable was added to tasks component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
+import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,7 +8,13 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {
+  }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
 }
