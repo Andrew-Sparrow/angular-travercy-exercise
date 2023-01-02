5c86e8c14bdfea84f87a162d48f6625e2dd5d986
Andrew <8911102@gmail.com>
2023-01-02 22:21:31 +0500

    ability to get data from json-server was added

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
 import { Task } from '../Task';
-import { Observable, of } from 'rxjs'
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
 
 
 @Injectable({
@@ -9,10 +9,11 @@ import { Observable, of } from 'rxjs'
 })
 
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
