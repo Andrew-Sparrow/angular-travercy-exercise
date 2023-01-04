408b7a86b1f94376993dec4a15a3787adcc3860d
Andrew <8911102@gmail.com>
2023-01-04 23:25:48 +0500

    functionality to add new task was added

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -33,4 +33,8 @@ export class TaskService {
 
     return this.http.put<Task>(url, task, httpOptions);
   }
+
+  addTaskService(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
 }
