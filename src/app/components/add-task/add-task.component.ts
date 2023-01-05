f8651b4d2ed843393a29a0dadf1177a41bfc650f
Andrew <8911102@gmail.com>
2023-01-05 17:20:18 +0500

    ability to show/hide add new task form was developed

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -4,6 +4,9 @@ import {
   Output,
   EventEmitter
 } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { UiService } from 'src/app/services/ui.service';
 
 import { Task } from 'src/app/Task';
 
@@ -18,6 +21,14 @@ export class AddTaskComponent {
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   onSubmit() {
     if (!this.text) {
